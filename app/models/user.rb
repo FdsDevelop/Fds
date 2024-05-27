61eb2a23b735300fa4f044c3d3a66c5bcd51ef12
HuRuiSheng <2417954265@qq.com>
2024-05-27 10:10:33 +0800

    1. 增加了用户名校验，用户名不允许重复

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :user_name, presence: { message: "can't be blank" }
+  validates :user_name, uniqueness: true
 end
